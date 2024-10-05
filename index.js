fc2c8a0ef970f84018caf19877975ede76a865b6
viviana39 <viviinformattyc@gmail.com>
2024-10-05 20:08:49 +0200

    creado podex y nuevos pokemon

--- index.js
+++ index.js
@@ -39,9 +39,39 @@ class PokemonPsi extends Pokemon {
     }
 }
 
+
+    class PokemonVolador extends Pokemon {
+        constructor(nombre = "Voladores", poder = "rafaga de viento") {
+            super(nombre);
+            this.poder = poder;
+        }
+    
+        atacar() {
+            console.log(`${this.nombre} usó el poder ${this.poder}`);
+        }
+    }
+
+class Pokemonvenenoso extends Pokemon {
+    constructor(nombre = "veneno", poder = "mata al instante"){
+        super(nombre);
+        this.poder = poder;
+    }
+    
+    atacar(){
+        console.log(`${this.nombre} usó el poder ${this.poder}`);
+       
+    }
+}
+
 class Pokedex {
     constructor(color = "SIN_COLOR") {
         this.color = color
+
+class pokedex {
+    constructor (color = "Oceano"){
+        this.color =color 
+    }
+}
 
         // COMPOSICION: El objeto Pokedex se forma de objetos Pokemon
         this.pokediccionario = [
@@ -49,10 +79,21 @@ class Pokedex {
             new Pokemon("Bulbasaur"),  // pos: 1
             new Pokemon(),             // pos: 2
             new Pokemon(),             // pos: 3
+            new Pokemon("pidgey"),
+            new Pokemon("hoothoot"),
+            new Pokemon("weezing"),
+            new Pokemon("toxicoak"),
+            new Pokemon("muk")
         ]
 
         console.log("NUEVA Pokedex CREADA:")
         this.listar()
+
+
+        console.log("NUEVA Pokedex de viento CREADA:")
+        this.listar()
+
+        console.log("NUEVA Pokedex Venenosa")
     }
 
     listar() {
@@ -66,6 +107,7 @@ myDragoncito.describir()
 myDragoncito.atacar()
 console.log("")
 console.log("")
+
 miTortugita = new Pokemon("Squirtel")
 miTortugita.describir()
 miTortugita.atacar()
@@ -80,14 +122,35 @@ miPsiloko = new PokemonPsi("LokoMon", "Enloquece")
 miPsiloko.describir()
 miPsiloko.atacar()
 
+let mypokevolador = new PokemonVolador("hoothoot","rafaga de viento")
+mypokevolador.describir()
+mypokevolador.atacar()
+console.log("");
+console.log("");
+
+
+let mypokemon = new Pokemonvenenoso("hoothoot","rafaga de viento")
+mypokemon.describir()
+mypokemon.atacar()
+console.log("");
+console.log("");
+
+
+
 const miLibroAzul = new Pokedex("azul")
 const miLibroRosita = new Pokedex("rosa")
+const miLibroOceano = new Pokedex("Oceano")
+const miLibroVerde = new Pokedex("verde")
 miLibroAzul.pokediccionario[2].nombre = "Pikachu"
 miLibroRosita.pokediccionario[2].nombre = "Squirtle"
+miLibroOceano.pokediccionario[4].nombre = "hoothoot"
+miLibroVerde.pokediccionario[7].nombre = "toxicoak"
 
 console.log("********************************************")
 miLibroAzul.listar()
 miLibroRosita.listar()
+miLibroOceano.listar()
+miLibroVerde.listar()
 
 // Ejercicio Sencillo: hacer una pokedex con pokemons de varios tipos
 // Ejercicio Facil: Hacer una clase PokemonVolador. PokemonFuegoVolador (charizard)
